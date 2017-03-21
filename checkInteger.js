11bc33e52444159dbf2372f829430c4ed5098a71
Eric Brovko <Eric Brovko>
2017-03-21 11:22:47 +0200

    removed to files

--- checkInteger.js
+++ checkInteger.js
@@ -2,7 +2,9 @@
 * Tasks for self-education
 * @description: diffetent task for self-education 
 */
+
 /* Create function, which is checking integer */
+
 /**
 * @hasInt function
 * @description: check integer
@@ -52,6 +54,8 @@ console.log(isInt("1a"));      // true
 console.log(isInt("4e2a"));    // true
 console.log(isInt("4e..1"));   // true
 console.log(isInt("4e.1."));   // true
+
+
 /**
 * @hasInt chasIntByRegExp
 * @description: check integer
@@ -107,24 +111,3 @@ console.log(chasIntByRegExp("1a"));      // true
 console.log(chasIntByRegExp("4e2a"));    // true
 console.log(chasIntByRegExp("4e..1"));   // true
 console.log(chasIntByRegExp("4e.1."));   // true
-
-
-// /* Difference beetwen Get and Post requests */
-var GET_NOTES = {
-  1: 'Requests data from a specified resource', // get params
-  2: 'can be cached and bookmarked',
-  3: 'never be used when dealing with sensitive data',
-  4: 'length restrictions', // around 2000 characters
-  5: 'should be used only to retrieve data'
-};
-var  POST_NOTES = {
-  1: 'Submits data to be processed to a specified resource', // query string is sent in the HTTP message body
-  2: 'never cached',
-  3: 'don`t remain in the browser history',
-  4: 'can`t be bookmarked',
-  5: 'no restrictions on data length'
-};
-// /* Idempotent */
-// /* term idempotent is used more comprehensively to describe an operation 
-// hat will produce the same results if executed once or multiple times
-// example - Get request. Same request === same results */
